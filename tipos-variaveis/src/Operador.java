a693ed0bce8d86f7ee45c55847f214df89fefd00
thaliaangelo <thaliagomes3009@gmail.com>
2022-12-18 14:12:28 -0300

    commit secundário

---
+++ tipos-variaveis/src/Operador.java
@@ -0,0 +1,11 @@
+public class Operador {
+    public static void main(String[] args){
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String result = a == b ? "true" : "false";
+
+        System.out.println(result);
+    }
+}
